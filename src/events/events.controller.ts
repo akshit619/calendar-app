73dcd17db166126977b12318a6113f885fb11d52
akshit619 <akshitbarnwal2001@gmail.com>
2025-06-24 14:02:48 +0530

    feat(events): expose CRUD endpoints via controller

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import { 
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Post,
+    Put, 
+} from '@nestjs/common';
+
+import {EventsService} from './events.service';
+import {Event} from './event.model';
 
 @Controller('events')
-export class EventsController {}
+export class EventsController {
+    constructor(private readonly eventsService: EventsService) {}
+
+    @Get()
+    findAll(): Event[]{
+        return this.eventsService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string): Event {
+        return this.eventsService.findOne(id);
+    }
+
+    @Post()
+    create(@Body() body: Partial<Event>): Event {
+        return this.eventsService.create(body);
+    
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string, @Body() body: Partial<Event>): Event {
+        return this.eventsService.update(id, body);
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: string): void {
+        return this.eventsService.delete(id);
+    }
+}
