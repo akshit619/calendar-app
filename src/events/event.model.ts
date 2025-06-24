f56d1b0b63a4bf306e010775d7c8f8bfb84c52d2
akshit619 <akshitbarnwal2001@gmail.com>
2025-06-24 20:22:24 +0530

    remove merge slop

--- src/events/event.model.ts
+++
@@ -1,9 +0,0 @@
-export class Event {
-    id: string;
-    title: string;
-    description?: string;
-    datetime: string;
-    imageUrl?: string;
-    videoUrl?: string;
-    createdAt: string;
-}
